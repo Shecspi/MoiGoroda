7bebe1b4dc2b5248bcb3adf07ec229c31c072a09
Shecspi <shecspi@yandex.ru>
2022-12-03 13:37:37 +0300

    Add a counter Yandex Metrika.

--- MoiGoroda/context_processors.py
+++ MoiGoroda/context_processors.py
@@ -7,8 +7,8 @@ environ.Env.read_env()
 def general_settings(request):
     context = {
         'SITE_NAME': env('SITE_NAME'),
-
-        'API_YANDEX_MAP': env('API_YANDEX_MAP')
+        'API_YANDEX_MAP': env('API_YANDEX_MAP'),
+        'YANDEX_METRIKA': env('YANDEX_METRIKA')
     }
 
     return context

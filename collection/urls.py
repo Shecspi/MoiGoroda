5a2d66dc2ef79e868e27d09ff1976b5f6e9811d6
Shecspi <shecspi@yandex.ru>
2022-12-03 13:52:21 +0300

    Remove app collection

--- collection/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from collection import views
-
-urlpatterns = [
-    path('', views.something, name='something')
-]
--- collection/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-
-# Create your views here.
-def something(request):
-    return HttpResponse('Everything is ok :)')

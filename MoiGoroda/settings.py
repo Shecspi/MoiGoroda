7e0485e72fa4e37fac87435ebadf526a98df3866
Shecspi <shecspi@yandex.ru>
2023-04-20 14:28:52 +0300

    Небольшие изменения дизайна region/cities_by_region__list.html

--- MoiGoroda/settings.py
+++ MoiGoroda/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'debug_toolbar',
     'crispy_forms',
     'crispy_bootstrap5',

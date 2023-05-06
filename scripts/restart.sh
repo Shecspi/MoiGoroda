94b16424a0fdb1b0a8162f46cc3b03ba938c9578
Shecspi <shecspi@yandex.ru>
2023-05-06 15:55:03 +0300

    Small fixes

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/expect
-# Скрипт перезапускает Gunicorn
+# Скрипт обновляет конфигурацию systemd и перезапускает Gunicorn
 set timeout -1
 spawn sudo systemctl daemon-reload
 expect "password"

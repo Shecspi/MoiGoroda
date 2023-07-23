3d77636ac629a33eb263874c6b59012cfda87ebb
Shecspi <shecspi@yandex.ru>
2023-07-23 12:07:08 +0300

    Update tests for region's models.

--- tests/region/models/test__region__models.py
+++ tests/region/models/test__region__models.py
@@ -43,7 +43,7 @@ def test__region__unique_iso3166(setup_db):
     with pytest.raises(django.db.utils.IntegrityError) as exc:
         Region.objects.create(
             area=Area.objects.get(id=1),
-            title='Адыгея',
+            title='Адыгея 2',
             type='R',
             iso3166='RU-AD'
         )
@@ -60,16 +60,57 @@ def test__region__unique_title_and_type(setup_db):
             area=Area.objects.get(id=1),
             title='Адыгея',
             type='R',
-            iso3166='RU-AD_UNIQUE'
+            iso3166='TEST'
         )
 
 
 @pytest.mark.django_db
-def test__region__correct_records(setup_db):
+def test__region__same_title(setup_db):
     """
-    Тестирует корректность заполнения полей таблицы.
-    Для республик есть 2 формата отображения - со словом "республик" и без него.
+    Записи с одинаковым полем `title`, но разным полем `type` могут быть созданы.
     """
-    assert Region.objects.get(id=2).title == 'Краснодарский'
-    assert Region.objects.get(id=2).type == 'K'
-    assert Region.objects.get(id=2).iso3166 == 'RU-KDA'
+    assert Region.objects.create(
+        area=Area.objects.get(id=1),
+        id=10,
+        title='Адыгея',
+        type='O',
+        iso3166='TEST2'
+    )
+
+
+@pytest.mark.django_db
+def test__region__same_type(setup_db):
+    """
+    Записи с одинаковым полем `type`, но разным полем `title` могут быть созданы.
+    """
+    assert Region.objects.create(
+        area=Area.objects.get(id=1),
+        id=20,
+        title='Адыгея2',
+        type='R',
+        iso3166='TEST3'
+    )
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    'region', (
+        (1, 'Адыгея', 'R', 'RU-AD', 'Адыгея'),
+        (2, 'Краснодарский', 'K', 'RU-KDA', 'Краснодарский край'),
+        (3, 'Волгоградская', 'O', 'RU-VGG', 'Волгоградская область'),
+        (4, 'Севастополь', 'G', 'RU-SEV', 'Севастополь'),
+        (5, 'Еврейская', 'AOb', 'RU-YEV', 'Еврейская автономная область'),
+        (6, 'Чукотский', 'AOk', 'RU-CHU', 'Чукотский автономный округ'),
+        (7, 'Чеченская республика', 'R', 'RU-CE', 'Чеченская республика')
+    )
+)
+def test__region__correct_records(setup_db: None, region: tuple):
+    """
+    Тестирует корректность заполнения полей таблицы, а также работу метода __str__().
+    Этот метод должен производить форматирование в зависимости от типа субъекта.
+    """
+    queryset = Region.objects.get(id=region[0])
+    assert queryset.title == region[1]
+    assert queryset.type == region[2]
+    assert queryset.iso3166 == region[3]
+    assert str(queryset) == region[4]

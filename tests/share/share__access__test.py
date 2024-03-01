a117f4565baa7e9a8839b43109b877d4cd731bb7
Shecspi <shecspi@yandex.ru>
2024-03-01 15:36:50 +0300

    Добавлены тесты на парсинг HTML-кода

--- tests/share/share__access__test.py
+++ tests/share/share__access__test.py
@@ -1,30 +1,7 @@
-
-
 import pytest
 from django.urls import reverse
 
-from account.models import ShareSettings
-
-
-def create_user(user_id: int, django_user_model):
-    return django_user_model.objects.create_user(id=user_id, username=f'username{user_id}', password='password')
-
-
-def create_permissions_in_db(user_id, permissions):
-    setting = ShareSettings(
-        user_id=user_id,
-        can_share=permissions[0],
-        can_share_dashboard=permissions[1],
-        can_share_city_map=permissions[2],
-        can_share_region_map=permissions[3]
-    )
-    setting.save()
-
-
-@pytest.fixture
-def setup(client, django_user_model):
-    user1 = create_user(1, django_user_model)
-    user2 = create_user(2, django_user_model)
+from tests.share.conftest import create_permissions_in_db
 
 
 @pytest.mark.django_db
@@ -119,3 +96,22 @@ def test__access_has_no_permissions_with_db_entry(setup, client, settings, reque
         else:
             response = client.get(reverse('share', kwargs={'pk': 1}), follow=True)
         assert response.status_code == 200
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    'requested_page, status_code', (
+        ('', 200),
+        ('city_map', 200),
+        ('region_map', 200),
+        ('sdfsdf', 404)
+    )
+)
+def test__requested_page(setup, client, requested_page, status_code):
+    create_permissions_in_db(1, (True, True, True, True))
+    if requested_page:
+        response = client.get(reverse('share', kwargs={'pk': 1, 'requested_page': requested_page}))
+    else:
+        response = client.get(reverse('share', kwargs={'pk': 1}))
+
+    assert response.status_code == status_code

79eed9ba2ad6fabbd0e8e7b9e8068fb5196238e4
Shecspi <shecspi@yandex.ru>
2024-05-07 20:20:14 +0300

    Форматирование с помощью ruff

--- news/migrations/0003_news_users_read.py
+++ news/migrations/0003_news_users_read.py
@@ -5,7 +5,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('news', '0002_alter_news_created_alter_news_last_modified'),
--- news/repository.py
+++ news/repository.py
@@ -4,6 +4,4 @@ from news.models import News
 
 
 def annotate_news_with_number_of_users_who_read_news(queryset: QuerySet[News]) -> QuerySet[News]:
-    return queryset.annotate(
-        number_of_users_who_read_news=Count('users_read')
-    )
+    return queryset.annotate(number_of_users_who_read_news=Count('users_read'))

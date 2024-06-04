03c28aaecc518d10111dba2e8bdbc45fd314da34
Shecspi <shecspi@yandex.ru>
2024-06-04 14:27:07 +0300

    Реализована логика переключения кнопки 'Подписаться'

--- static/js/subscribe_api.js
+++ static/js/subscribe_api.js
@@ -3,13 +3,32 @@ async function send_to_server() {
     const data = {
         'subscribe': true
     }
-
+    
     let response = await fetch(url);
-    if (response.ok) { // если HTTP-статус в диапазоне 200-299
-        // получаем тело ответа (см. про этот метод ниже)
-        let json = await response.json();
-        alert(json.status);
-      } else {
-        alert("Ошибка HTTP: " + response.status);
+    if (response.ok) {
+      const subscribe_button = document.getElementById('subscribe_button')
+      const unsubscribe_button = document.getElementById('unsubscribe_button')
+
+      let json = await response.json();
+      if (json.status == 'subscribed') {
+        subscribe_button.hidden = true;
+        unsubscribe_button.hidden = false;
+
+        const element = document.getElementById('toast_subscribe_api_ok');
+        const toast = new bootstrap.Toast(element)
+        toast.show()
       }
+      else if (json.status == 'unsubscribed') {
+        subscribe_button.hidden = false;
+        unsubscribe_button.hidden = true;
+
+        const element = document.getElementById('toast_unsubscribe_api_ok');
+        const toast = new bootstrap.Toast(element)
+        toast.show()
+      }
+    } else {
+      alert("Ошибка отправки HTTP-запроса");
+    }
+
+    return false;
 }

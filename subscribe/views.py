e7890f7c43582f93e0d377ad623224f1bd4bc798
Shecspi <shecspi@yandex.ru>
2024-06-08 14:39:54 +0300

    Настроено взаимодействие кнопки 'Подписаться' с сервером

--- subscribe/views.py
+++ subscribe/views.py
@@ -1,7 +1,23 @@
+import json
+
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 
 
-@login_required()
+@require_POST
+@login_required
 def save(request):
-    return JsonResponse({'status': 'subscribed'})
+    data = json.loads(request.body.decode('utf-8'))
+    from_id = data.get('from_id')
+    to_id = data.get('to_id')
+    action = data.get('action')
+
+    if action == 'subscribe':
+        print(action, from_id, to_id)
+        return JsonResponse({'status': 'subscribed'})
+    elif action == 'unsubscribe':
+        print(action, from_id, to_id)
+        return JsonResponse({'status': 'unsubscribed'})
+    else:
+        return JsonResponse({'status': 'error'})

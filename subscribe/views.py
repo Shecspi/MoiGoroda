f984fe9a1fdca441140f4a43aab39a0dcfd018ff
Shecspi <shecspi@yandex.ru>
2024-06-12 15:27:00 +0300

    Добавлена проверка возможности совершить подписку

--- subscribe/views.py
+++ subscribe/views.py
@@ -1,8 +1,24 @@
 import json
+from enum import StrEnum, auto
 
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
+
+from subscribe.repository import can_subscribe
+
+
+class Action(StrEnum):
+    subscribe = auto()
+    unsubscribe = auto()
+
+
+def is_user_has_permission_to_change_subscription(from_id: int, user_id: int):
+    return from_id == user_id
+
+
+def has_user_allowed_to_subscribe_to_himself(to_id: int):
+    return can_subscribe(to_id)
 
 
 @require_POST
@@ -12,6 +28,11 @@ def save(request):
     from_id = data.get('from_id')
     to_id = data.get('to_id')
     action = data.get('action')
+
+    if not is_user_has_permission_to_change_subscription(from_id, request.user.id):
+        return JsonResponse(data={'status': 'forbidden'}, status=403)
+    if not has_user_allowed_to_subscribe_to_himself(to_id):
+        return JsonResponse(data={'status': 'forbidden'}, status=403)
 
     if action == 'subscribe':
         print(action, from_id, to_id)

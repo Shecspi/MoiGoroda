b237300c6ef3f1fe32b1e9138ce26e31b86aab44
Shecspi <shecspi@yandex.ru>
2024-06-16 14:40:59 +0300

    Реализован механизм сохранения подписок в БД

--- subscribe/repository.py
+++ subscribe/repository.py
@@ -7,11 +7,22 @@ Licensed under the Apache License, Version 2.0
 ----------------------------------------------
 """
 
+from django.contrib.auth.models import User
+
 from account.models import ShareSettings
 from subscribe.models import Subscribe
 
 
-def is_subscribed(subscribe_from_id: int, subscribe_to_id: int):
+def is_user_exists(id: int) -> bool:
+    try:
+        User.objects.get(pk=id)
+    except User.DoesNotExist:
+        return False
+    else:
+        return True
+
+
+def is_subscribed(subscribe_from_id: int, subscribe_to_id: int) -> bool:
     try:
         Subscribe.objects.get(subscribe_from=subscribe_from_id, subscribe_to=subscribe_to_id)
     except Subscribe.DoesNotExist:
@@ -20,8 +31,16 @@ def is_subscribed(subscribe_from_id: int, subscribe_to_id: int):
         return True
 
 
-def can_subscribe(to_id: int):
+def can_subscribe(to_id: int) -> bool:
     try:
         return ShareSettings.objects.get(user_id=to_id).can_subscribe
     except ShareSettings.DoesNotExist:
         return False
+
+
+def add_subscription(from_id: int, to_id: int) -> None:
+    Subscribe.objects.create(subscribe_from_id=from_id, subscribe_to=User.objects.get(pk=to_id))
+
+
+def delete_subscription(from_id: int, to_id: int) -> None:
+    Subscribe.objects.get(subscribe_from_id=from_id, subscribe_to_id=to_id).delete()

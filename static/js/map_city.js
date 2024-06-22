d24ce4fc4c1775e1509c4b0ca49bc5d9fd33b38f
Shecspi <shecspi@yandex.ru>
2024-06-22 13:30:46 +0300

    Исправлена ошибка центрирования карты

--- static/js/map_city.js
+++ static/js/map_city.js
@@ -1,6 +1,6 @@
-function createMap(center_y, center_x, zoom) {
+function createMap(center_lat, center_lon, zoom) {
     var myMap = new ymaps.Map("map", {
-        center: [center_y, center_x],
+        center: [center_lat, center_lon],
         zoom: zoom,
         controls: ['fullscreenControl', 'zoomControl', 'rulerControl']
     }, {
@@ -28,25 +28,25 @@ function calculateCenterCoordinates(visited_cities) {
     if (visited_cities.length > 0) {
         // Высчитываем центральную точку карты.
         // Ей является средняя координата всех городов, отображённых на карте.
-        let array_x = Array();
-        let array_y = Array();
+        let array_lon = Array();
+        let array_lat = Array();
         let zoom = 0;
 
         // Добавляем все координаты в один массив и находим большее и меньшее значения из них,
         // а затем вычисляем среднее, это и будет являться центром карты.
         for (let i = 0; i < visited_cities.length; i++) {
-            array_y.push(parseFloat(visited_cities[i][0]));
-            array_x.push(parseFloat(visited_cities[i][1]));
+            array_lat.push(parseFloat(visited_cities[i][0]));
+            array_lon.push(parseFloat(visited_cities[i][1]));
         }
-        let max_x = Math.max(...array_x);
-        let min_x = Math.min(...array_x);
-        let max_y = Math.max(...array_y);
-        let min_y = Math.min(...array_y);
-        average_x = (max_x + min_x) / 2;
-        average_y = (max_y + min_y) / 2;
+        let max_lon = Math.max(...array_lon);
+        let min_lon = Math.min(...array_lon);
+        let max_lat = Math.max(...array_lat);
+        let min_lat = Math.min(...array_lat);
+        average_lon = (max_lon + min_lon) / 2;
+        average_lat = (max_lat + min_lat) / 2;
 
         // Меняем масштаб карты в зависимости от расположения городов
-        let diff = max_y - min_y;
+        let diff = max_lat - min_lat;
         if (diff <= 1) {
             zoom = 8;
         } else if (diff > 1 && diff <= 2) {
@@ -58,19 +58,17 @@ function calculateCenterCoordinates(visited_cities) {
         } else {
             zoom = 4;
         }
-
-        return [average_x, average_y, zoom];
+        return [average_lat, average_lon, zoom];
     }
     else {
-        return [37.588144, 55.733842, 7];
+        return [56.831534, 50.987919, 5];
     }
 }
 
-function showMap() {
-    const [center_x, center_y, zoom] = calculateCenterCoordinates(visited_cities);
-    myMap = createMap(center_x, center_y, zoom);
+function init() {
+    const [center_lat, center_lon, zoom] = calculateCenterCoordinates(visited_cities);
+    myMap = createMap(center_lat, center_lon, zoom);
     addCitiesOnMap(visited_cities, myMap);
 }
 
-
-ymaps.ready(showMap);
+ymaps.ready(init);

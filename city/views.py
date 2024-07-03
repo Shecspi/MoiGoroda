2263f0e868a6c0ce76dc78e3fe36516cf0326f3c
Shecspi <shecspi@yandex.ru>
2024-07-03 22:17:53 +0300

    Добавлена проверка настроек пользователей перед тем, как вернуть их посещённые города

--- city/views.py
+++ city/views.py
@@ -25,6 +25,7 @@ from django.views.generic import ListView, CreateView, DeleteView, UpdateView, D
 from pydantic import ValidationError
 from rest_framework.permissions import IsAuthenticated
 
+from account.models import ShareSettings
 from city.forms import VisitedCity_Create_Form
 from city.models import VisitedCity, City
 import city.structs as structs
@@ -432,13 +433,45 @@ class GetVisitedCitiesFromSubscriptions_API(generics.ListAPIView):
     permission_classes = (IsAuthenticated,)
     http_method_names = ['get']
 
-    def get_queryset(self):
+    def __init__(self):
+        # Список ID пользователей, у которых необходимо вернуть посещённые города
+        self.user_id: list = []
+
+        super().__init__()
+
+    def get(self, *args, **kwargs):
         input_data = self.request.GET.get('data')
         json_data = json.loads(input_data)
-        user_ids = json_data.get('ids')
+        user_id = json_data.get('ids')
 
+        if not self.request.user.is_superuser:
+            # Убираем из списка ID тех пользователей, которые не разрешили подписываться на себя.
+            # Вообще это нештатная ситуация, но теоритически возможная, когда пользователь запретил
+            # подписываться после того, как была открыта страница с картой, но до того, как запрос пришёл на сервер.
+            for id in user_id:
+                try:
+                    user_settings = ShareSettings.objects.get(id=id)
+                except ShareSettings.DoesNotExist:
+                    logger.warning(
+                        self.request,
+                        f'(Share settings) Attempt to get a list of the cities of a user who did not change initial settings',
+                    )
+                else:
+                    if user_settings.can_subscribe:
+                        self.user_id.append(id)
+                    else:
+                        logger.warning(
+                            self.request,
+                            f'(Share settings) Attempt to get a list of the cities of a user who did not allow it',
+                        )
+        else:
+            self.user_id = user_id
+
+        return super().get(*args, **kwargs)
+
+    def get_queryset(self):
         return get_visited_cities_many_users(
-            user_ids,
+            self.user_id,
             ('city', 'user'),
             [
                 'user__username',

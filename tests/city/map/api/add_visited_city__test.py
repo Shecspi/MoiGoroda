c5257ee2d79bb7395203f84db30c336d45fd88ac
Shecspi <shecspi@yandex.ru>
2024-07-08 21:33:47 +0300

    Написан тест для проверки корректности передаваемых данных (нужно заполнить его данными)

--- tests/city/map/api/add_visited_city__test.py
+++ tests/city/map/api/add_visited_city__test.py
@@ -5,6 +5,8 @@ Licensed under the Apache License, Version 2.0
 
 """
 
+import json
+
 import pytest
 
 from django.urls import reverse
@@ -63,3 +65,61 @@ def access_by_PATCH_is_prohibited__test(setup_db_without_visited_cities, caplog,
     assert caplog.records[0].levelname == 'WARNING'
     assert caplog.records[0].getMessage() == 'Method Not Allowed: /api/city/visited/add'
     assert response.status_code == 405
+
+
+@pytest.mark.django_db
+def access_by_POST_for_guest_is_prohibited__test(setup_db_without_visited_cities, caplog, client):
+    response = client.get(reverse('api__add_visited_city'))
+
+    assert caplog.records[0].levelname == 'WARNING'
+    assert caplog.records[0].getMessage() == 'Forbidden: /api/city/visited/add'
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    'request_data, correct_response_data, log_message',
+    (
+        (
+            {
+                'city': 1,
+                'date_of_visit': '2024-08-01',
+                'has_magnet': True,
+                'impression': 'impression',
+                'rating': 3,
+            },
+            {
+                'status': 'success',
+                'city': {
+                    'id': 1,
+                    'city': 1,
+                    'city_title': 'Город 1',
+                    'region_title': 'Регион 1 область',
+                    'date_of_visit': '2024-08-01',
+                    'has_magnet': True,
+                    'impression': 'impression',
+                    'rating': 3,
+                },
+            },
+            '(API: Add visited city) The visited city has been successfully added from unknown location   /api/city/visited/add',
+        ),
+    ),
+)
+def test__response_with_correct_request_data(
+    setup_db_without_visited_cities,
+    caplog,
+    client,
+    request_data,
+    correct_response_data,
+    log_message,
+):
+    client.login(username='username1', password='password')
+    response = client.post(
+        reverse('api__add_visited_city'),
+        data=json.dumps(request_data),
+        content_type='application/json',
+    )
+    response_data = json.loads(response.content.decode())
+    assert response_data == correct_response_data
+    assert caplog.records[0].levelname == 'INFO'
+    assert caplog.records[0].getMessage() == log_message

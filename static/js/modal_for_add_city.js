ca2acb873d8363f5a1adafe4752bf60927c4b951
Shecspi <shecspi@yandex.ru>
2024-08-02 14:16:55 +0300

    Добавлено всплывающее уведомление об успешном добавлении города

--- static/js/modal_for_add_city.js
+++ static/js/modal_for_add_city.js
@@ -1,18 +1,8 @@
-function open_modal_for_add_city(city, city_id, region_title) {
-    const modal = new bootstrap.Modal(document.getElementById('modal_add_city'), {
-        'backdrop': true
-    });
-    const el_city_title = document.getElementById('city-title-in-modal');
-    const el_region_title = document.getElementById('region-title-in-modal');
-    const el_city_id = document.getElementById('city-id');
-
-    el_city_title.innerText = city;
-    el_region_title.innerText = region_title;
-    el_city_id.setAttribute('value', city_id);
-    modal.toggle();
-}
-
+const modal = new bootstrap.Modal(document.getElementById('modal_add_city'), {
+    'backdrop': true
+});
 const form = document.getElementById('form-add-city');
+
 form.addEventListener('submit', event => {
     event.preventDefault();
 
@@ -33,9 +23,26 @@ form.addEventListener('submit', event => {
             return response.json()
         })
         .then((data) => {
+            modal.toggle();
+
             console.log(data);
+            const element = document.getElementById('toast-success');
+            const toast = new bootstrap.Toast(element);
+            document.getElementById('city-title-in-toast').innerText = data.city.city_title;
+            toast.show();
         })
         .catch((err) => {
             console.log(err);
         })
     });
+
+function open_modal_for_add_city(city, city_id, region_title) {
+    const el_city_title = document.getElementById('city-title-in-modal');
+    const el_region_title = document.getElementById('region-title-in-modal');
+    const el_city_id = document.getElementById('city-id');
+
+    el_city_title.innerText = city;
+    el_region_title.innerText = region_title;
+    el_city_id.setAttribute('value', city_id);
+    modal.toggle();
+}

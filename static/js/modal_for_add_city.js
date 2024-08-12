3fad83b14a2a3015207087b849d2c7c05a8c9b22
Shecspi <shecspi@yandex.ru>
2024-08-12 18:43:45 +0300

    Реализовано склонение слов 'посещено' и 'город' в тулбаре на странице общей карты

--- static/js/modal_for_add_city.js
+++ static/js/modal_for_add_city.js
@@ -16,11 +16,9 @@ form.addEventListener('submit', event => {
     const url = button.dataset.url;
 
     fetch(url, {
-        method: 'POST',
-        headers: {
+        method: 'POST', headers: {
             'X-CSRFToken': getCookie("csrftoken")
-        },
-        body: formData
+        }, body: formData
     })
         .then((response) => {
             if (!response.ok) {
@@ -42,6 +40,7 @@ form.addEventListener('submit', event => {
             toast.show();
 
             actions.updatePlacemark(data.city.city);
+            change_qty_of_visited_cities_in_toolbar();
         })
         .catch((err) => {
             console.log(err);
@@ -60,4 +59,44 @@ function open_modal_for_add_city(city, city_id, region_title) {
     el_region_title.innerText = region_title;
     el_city_id.setAttribute('value', city_id);
     modal.toggle();
+}
+
+function change_qty_of_visited_cities_in_toolbar() {
+    const declensionVisitedElement = document.getElementById('declension-visited');
+    const declensionQtyVisitedElement = document.getElementById('declension-qty-visited');
+    const declensionVisitedCityElement = document.getElementById('declension-visited-city');
+
+    const oldQty = declensionQtyVisitedElement.textContent;
+    const newQty = Number(oldQty) + 1;
+    declensionQtyVisitedElement.innerText = newQty.toString();
+
+    declensionVisitedElement.innerText = toTitleCase(declensionVisited(newQty))
+    declensionVisitedCityElement.innerText =
+        declensionVisitedCity(newQty);
+}
+
+function declensionVisited(newQty) {
+    if (newQty.toString().endsWith('1') && !newQty.toString().endsWith('11')) {
+        return 'посещён'
+    } else {
+        return 'посещено'
+    }
+}
+
+function declensionVisitedCity(newQty) {
+    const newQtyStr = newQty.toString();
+    if (
+        ((10 <= Number(newQtyStr.slice(-2))) && (Number(newQtyStr.slice(-2)) <= 20))
+        || (['5', '6', '7', '8', '9', '0'].includes(newQtyStr.slice(-1)))
+    ) {
+        return 'городов';
+    } else if (['2', '3', '4'].includes(newQtyStr.slice(-1))) {
+        return 'города'
+    } else if (newQtyStr.slice(-1) === '1') {
+        return 'город';
+    }
+}
+
+function toTitleCase(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1);
 }

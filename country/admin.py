439bba78781989bcbef4031a5e939b5531a4756e
Shecspi <shecspi@yandex.ru>
2024-08-16 14:01:10 +0300

    Определены модели приложения

--- country/admin.py
+++ country/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from country.models import PartOfTheWorld, Country, Location
+
+admin.site.register([PartOfTheWorld, Location, Country])
--- country/models.py
+++ country/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+
+class PartOfTheWorld(models.Model):
+    name = models.CharField(
+        max_length=20, unique=True, blank=False, null=False, verbose_name='Название'
+    )
+
+    class Meta:
+        verbose_name = 'Часть света'
+        verbose_name_plural = 'Части света'
+
+    def __str__(self):
+        return self.name
+
+
+class Location(models.Model):
+    name = models.CharField(
+        max_length=50, unique=True, blank=False, null=False, verbose_name='Название'
+    )
+    part_of_the_world = models.ForeignKey(
+        PartOfTheWorld, on_delete=models.CASCADE, null=True, blank=True
+    )
+
+    class Meta:
+        verbose_name = 'Расположение'
+        verbose_name_plural = 'Расположения'
+
+    def __str__(self):
+        return self.name
+
+
+class Country(models.Model):
+    name = models.CharField(
+        max_length=100, unique=True, blank=False, null=False, verbose_name='Название'
+    )
+    fullname = models.CharField(
+        max_length=100, unique=True, blank=True, null=True, verbose_name='Полное название'
+    )
+    code = models.CharField(max_length=2, unique=True, blank=False, null=False, verbose_name='Код')
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Страна'
+        verbose_name_plural = 'Страны'
+
+    def __str__(self):
+        return self.name

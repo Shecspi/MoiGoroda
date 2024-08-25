149a93540f94cd3cfb764789f1f8ff279d5b7fba
Shecspi <shecspi@yandex.ru>
2024-08-25 15:17:26 +0300

    Реализовано добавление страны на карте и изменение её окраски в зависимости от того, посещена она или нет

--- static/js/country_map.js
+++ static/js/country_map.js
@@ -1,17 +1,19 @@
-const fillColorVisitedCoutry = '#32b700';
-const fillColorNotVisitedCoutry = '#9a9a9a';
+const fillColorVisitedCountry = '#32b700';
+const fillColorNotVisitedCountry = '#9a9a9a';
 const fillOpacity = 0.6;
 const strokeColor = '#FFF';
 const strokeOpacity = 0.5;
+let allCountriesGoeObjects = new Map();
+let myMap;
 
 ymaps.ready(init);
 
 function init() {
-    var myMap = new ymaps.Map("map", {
+    myMap = new ymaps.Map("map", {
         center: [55.76, 37.64],
         zoom: 2
     });
-    fetch('/api/country/all')
+    const allCountryPromise = fetch('/api/country/all')
         .then((response) => {
             if (!response.ok) {
                 throw new Error(response.statusText)
@@ -19,52 +21,81 @@ function init() {
             return response.json();
         })
         .then((data) => {
-            ymaps.borders.load('001', {lang: 'ru', quality: 1}).then(function (geojson) {
-                let countries = new Map();
-                for (let i = 0; i < data.length; i++) {
-                    countries.set(data[i].code, data[i].name);
-                }
-                console.log('Всего стран в БД: ', countries);
-
-                for (let i = 0; i < geojson.features.length; i++) {
-                    let countryCode = geojson.features[i].properties.iso3166;
-                    let countryName = geojson.features[i].properties.name;
-                    let contentHeader = '<span class="fw-semibold">' + countryName + '</span>'
-                    let linkToAdd = `<hr><a href="#" onclick="add_country('${countryCode}')">Отметить страну как посещённую</a>`
-
-                    // Если такой страны нет в нашей БД, то пропускаем её и печатаем в консоль.
-                    // Если есть, то удаляем её из countries, чтобы в конце посмотреть,
-                    // какие страны из нашей БД не распечатались на карте.
-                    if (!countries.has(countryCode)) {
-                        console.log(`Страны "${countryName}" нет в нашей БД`);
-                        continue;
-                    } else {
-                        countries.delete(countryCode);
-                    }
-
-                    let geoObject = new ymaps.GeoObject(geojson.features[i], {
-                        fillColor: fillColorNotVisitedCoutry,
-                        fillOpacity: fillOpacity,
-                        strokeColor: strokeColor,
-                        strokeOpacity: strokeOpacity,
-                    });
-                    geoObject.properties.set({
-                        balloonContentHeader: contentHeader,
-                        balloonContent: linkToAdd
-                    });
-
-                    myMap.geoObjects.add(geoObject);
-                }
-
-                console.log('Страны, которых нет в Яндексе:', countries);
-            });
+            return data;
         });
+
+    const visitedCountryPromise = fetch('/api/country/visited')
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(response.statusText)
+            }
+            return response.json();
+        })
+        .then((data) => {
+            return data;
+        });
+
+    Promise.all([allCountryPromise, visitedCountryPromise]).then(([allCountries, visitedCountries]) => {
+        ymaps.borders.load('001', {lang: 'ru', quality: 1}).then(function (geojson) {
+            let countries = new Map();
+            for (let i = 0; i < allCountries.length; i++) {
+                countries.set(allCountries[i].code, allCountries[i].name);
+            }
+            console.log('Всего стран в БД: ', countries);
+
+            let visitedCountriesSet = new Set();
+            for (let i = 0; i < visitedCountries.length; i++) {
+                visitedCountriesSet.add(visitedCountries[i].code);
+            }
+
+            for (let i = 0; i < geojson.features.length; i++) {
+                let countryCode = geojson.features[i].properties.iso3166;
+                let countryName = geojson.features[i].properties.name;
+                const isVisited = visitedCountriesSet.has(countryCode);
+
+                // Если такой страны нет в нашей БД, то пропускаем её и печатаем в консоль.
+                // Если есть, то удаляем её из countries, чтобы в конце посмотреть,
+                // какие страны из нашей БД не распечатались на карте.
+                if (!countries.has(countryCode)) {
+                    console.log(`Страны "${countryName}" нет в нашей БД`);
+                    continue;
+                } else {
+                    countries.delete(countryCode);
+                }
+
+                let geoObject = addCountryOnMap(geojson.features[i], countryCode, countryName, isVisited);
+                allCountriesGoeObjects.set(countryCode, geoObject);
+            }
+
+            console.log('Страны, которых нет в Яндексе:', countries);
+        });
+    });
+}
+
+function addCountryOnMap(geojson, countryCode, countryName, isVisited) {
+    let contentHeader = '<span class="fw-semibold">' + countryName + '</span>'
+    let linkToAdd = `<hr><a href="#" onclick="add_country('${countryCode}')">Отметить страну как посещённую</a>`
+
+    let geoObject = new ymaps.GeoObject(geojson, {
+        fillColor: isVisited ? fillColorVisitedCountry : fillColorNotVisitedCountry,
+        // visitedCountriesSet.has(countryCode) ? fillColorVisitedCountry : fillColorNotVisitedCountry
+        fillOpacity: fillOpacity,
+        strokeColor: strokeColor,
+        strokeOpacity: strokeOpacity,
+    });
+    geoObject.properties.set({
+        balloonContentHeader: contentHeader,
+        balloonContent: linkToAdd
+    });
+    myMap.geoObjects.add(geoObject);
+
+    return geoObject;
 }
 
 function add_country(countryCode) {
     const url = document.getElementById('url_add_visited_country').dataset.url;
     const formData = new FormData();
-    formData.set('country', countryCode);
+    formData.set('code', countryCode);
 
     let response = fetch(url, {
         method: 'POST',
@@ -73,11 +104,19 @@ function add_country(countryCode) {
         },
         body: formData
     })
-    .then((response) => {
-        if (!response.ok) {
-            throw new Error(response.statusText)
-        }
-        return response.json()
-    })
-    .then((data) => {});
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(response.statusText)
+            }
+            return response.json()
+        })
+        .then((data) => {
+            const country = allCountriesGoeObjects.get(countryCode);
+
+            allCountriesGoeObjects.delete(countryCode);
+            myMap.geoObjects.remove(country);
+
+            let geoObject = addCountryOnMap(country, countryCode, 'countryName', true);
+            allCountriesGoeObjects.set(countryCode, geoObject);
+        });
 }

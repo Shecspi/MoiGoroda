c9892bde3265a353d43872e791a5086067657aa1
Shecspi <shecspi@yandex.ru>
2024-09-25 15:42:47 +0300

    Модель "Посещённые города" добавлена в админку

--- country/admin.py
+++ country/admin.py
@@ -9,6 +9,26 @@ Licensed under the Apache License, Version 2.0
 
 from django.contrib import admin
 
-from country.models import PartOfTheWorld, Country, Location
+from country.models import PartOfTheWorld, Country, Location, VisitedCountry
 
 admin.site.register([PartOfTheWorld, Location, Country])
+
+
+@admin.register(VisitedCountry)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'country',
+        'location',
+        'part_of_the_world',
+        'user',
+    )
+    ordering = ('country', 'user')
+
+    @admin.display(description='Расположение', ordering='country__location__name')
+    def location(self, obj):
+        return obj.country.location
+
+    @admin.display(description='Часть света', ordering='country__location__part_of_the_world__name')
+    def part_of_the_world(self, obj):
+        return obj.country.location.part_of_the_world

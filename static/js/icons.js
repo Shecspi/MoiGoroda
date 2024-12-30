3a6b55ee93566adf450a93f77515b8ff0551a2af
Shecspi <shecspi@yandex.ru>
2024-12-30 16:21:15 +0300

    Убрано смещение иконок на карте городов страны и карте городов региона

--- static/js/icons.js
+++ static/js/icons.js
@@ -14,7 +14,9 @@ export const icon_visited_pin = L.divIcon({
     className: 'custom-icon-visited-pin',
     html: '<i class="fa-solid fa-location-dot fs-3 icon-visited-pin" style="color: rgb(90, 170, 90) !important; text-shadow: 0 0 2px #333333;"></i>',
     iconSize: [21, 28],
-    anchor: [10.5, 28]
+    iconAnchor: [10.5, 28],
+    popupAnchor: [0, -28],
+    tooltipAnchor: [0, -28]
 });
 
 // Иконка для города, который не посетил ни пользователь, ни те, на кого он подписан
@@ -22,7 +24,10 @@ export const icon_not_visited_pin = L.divIcon({
     className: 'custom-icon-not_visited-pin',
     html: '<i class="fa-solid fa-location-dot fs-3 icon-not-visited-pin" style="color: rgb(210, 90, 90) !important; text-shadow: 0 0 2px #333333;"></i>',
     iconSize: [21, 28],
-    anchor: [10.5, 28]
+    anchor: [10.5, 28],
+    iconAnchor: [10.5, 28],
+    popupAnchor: [0, -28],
+    tooltipAnchor: [0, -28]
 });
 
 // Иконка для города, который был посещён пользователем и кем-то из тех, на кого он подписан
@@ -30,7 +35,10 @@ export const icon_together_pin = L.divIcon({
     className: 'custom-icon-together-pin',
     html: '<i class="fa-solid fa-location-dot fs-3 icon-together-pin" style="color: rgb(40, 130, 10) !important; text-shadow: 0 0 2px #333333;"></i>',
     iconSize: [21, 28],
-    anchor: [10.5, 28]
+    anchor: [10.5, 28],
+    iconAnchor: [10.5, 28],
+    popupAnchor: [0, -28],
+    tooltipAnchor: [0, -28]
 });
 
 // Иконка для города, который не был посещён пользователя, но посещён кем-то из тех, на кого он подписан
@@ -38,5 +46,8 @@ export const icon_subscription_pin = L.divIcon({
     className: 'custom-icon-subscription-pin',
     html: '<i class="fa-solid fa-location-dot fs-3  icon-subscription-pin" style="color: rgb(230, 150, 30) !important; text-shadow: 0 0 2px #333333;"></i>',
     iconSize: [21, 28],
-    anchor: [10.5, 28]
+    anchor: [10.5, 28],
+    iconAnchor: [10.5, 28],
+    popupAnchor: [0, -28],
+    tooltipAnchor: [0, -28]
 });

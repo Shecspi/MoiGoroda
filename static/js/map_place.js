74fbf215d5a258c17d619fb001e09a742094f52d
Shecspi <shecspi@yandex.ru>
2024-12-31 15:55:56 +0300

    Улучшен запрос в Nominatim

--- static/js/map_place.js
+++ static/js/map_place.js
@@ -2,55 +2,57 @@ import {create_map} from './map.js';
 import {icon_place_pin} from "./icons.js";
 
 const map = create_map([55.7520251, 37.61841444746334], 15);
+let marker = undefined;
 
 map.addEventListener('click', function(ev) {
     const lat = ev.latlng.lat;
     const lon = ev.latlng.lng;
 
-    ['administrative', 'poi', 'natural'].forEach((item) => {
-        console.log(item);
-        let url = `https://nominatim.openstreetmap.org/reverse?email=shecspi@yandex.ru&format=jsonv2&lat=${lat}&lon=${lon}&addressdetails=0`;
-        if (item === 'administrative') {
-            url += '&zoom=14'
-        } else if (item === 'poi') {
-            url += '&zoom=18&layer=poi'
-        } else if (item === 'natural') {
-            url += '&zoom=18&layer=natural';
-        }
+    let url = `https://nominatim.openstreetmap.org/reverse?email=shecspi@yandex.ru&format=jsonv2&lat=${lat}&lon=${lon}&addressdetails=0&zoom=18&layer=natural,poi`;
 
-        fetch(url)
-            .then(response => response.json())
-            .then(data => {
-                let marker;
-                if (data.hasOwnProperty('error')) {
-                    marker = L.marker(
-                        [lat, lon],
-                        {
-                            icon: icon_place_pin,
-                            draggable: true,
-                            bounceOnAdd: true
-                        }
-                    ).addTo(map);
-                    marker.bindTooltip('Неизвестный объект', {permanent: true, direction: 'top'});
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            if (marker !== undefined) {
+                map.removeLayer(marker);
+            }
+
+            if (data.hasOwnProperty('error')) {
+                marker = L.marker(
+                    [lat, lon],
+                    {
+                        icon: icon_place_pin,
+                        draggable: true,
+                        bounceOnAdd: true
+                    }
+                ).addTo(map);
+                marker.bindTooltip('Неизвестный объект', {permanent: true, direction: 'top'});
+            } else {
+                marker = L.marker(
+                    [data.lat, data.lon],
+                    {
+                        icon: icon_place_pin,
+                        draggable: true,
+                        bounceOnAdd: true
+                    }
+                ).addTo(map);
+                // marker.bindTooltip(data.name, {permanent: true, direction: 'top'});
+                let content = '';
+                if (data.name !== '') {
+                    content = `<h5>${data.name}</h5>`;
+                } else if (data.display_name !== '') {
+                    content = `<h5>${data.display_name}</h5>`;
                 } else {
-                    marker = L.marker(
-                        [data.lat, data.lon],
-                        {
-                            icon: icon_place_pin,
-                            draggable: true,
-                            bounceOnAdd: true
-                        }
-                    ).addTo(map);
-                    // marker.bindTooltip(data.name, {permanent: true, direction: 'top'});
-                    let content = `<h5>${data.name}</h5>`;
-                    content += '<button class="btn btn-success btn-sm">Добавить</button>'
-                    marker.bindPopup(content);
-                    marker.openPopup();
-
-                    marker.on("dragend", function(e){
-                        console.log(e.target.getLatLng().toString());
-                    });
+                    content = `<h5>Неизвестный объект</h5>`;
                 }
-            });
-    });
+
+                content += '<button class="btn btn-success btn-sm">Добавить</button>'
+                marker.bindPopup(content);
+                marker.openPopup();
+
+                marker.on("dragend", function(e){
+                    console.log(e.target.getLatLng().toString());
+                });
+            }
+        });
 });

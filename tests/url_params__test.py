c1160275d9b1489331586209c89da2a9ade0d43a
Shecspi <shecspi@yandex.ru>
2025-02-27 13:39:38 +0300

    Переименованы тесты для make_url_params

--- tests/url_params__test.py
+++ tests/url_params__test.py
@@ -11,37 +11,37 @@ Licensed under the Apache License, Version 2.0
 from services.url_params import make_url_params
 
 
-def test__url_params_empty():
+def url_params_empty__test():
     assert make_url_params() == ''
 
 
-def test__url_params_filter_is_none():
+def url_params_filter_is_none__test():
     filter_value = None
     assert make_url_params(filter_value=filter_value) == ''
 
 
-def test__url_params_sort_is_none():
+def url_params_sort_is_none__test():
     sort_value = None
     assert make_url_params(sort_value=sort_value) == ''
 
 
-def test__url_params_only_filter():
+def url_params_only_filter__test():
     filter_value = 'new'
     assert make_url_params(filter_value=filter_value) == f'filter={filter_value}'
 
 
-def test__url_params_only_sort():
+def url_params_only_sort__test():
     sort_value = 'new'
     assert make_url_params(sort_value=sort_value) == f'sort={sort_value}'
 
 
-def test__url_params_filter_is_none_and_sort():
+def url_params_filter_is_none_and_sort__test():
     filter_value = None
     sort_value = 'new_sort'
     assert make_url_params(filter_value=filter_value, sort_value=sort_value) == f'sort={sort_value}'
 
 
-def test__url_params_filter_and_sort_is_none():
+def url_params_filter_and_sort_is_none__test():
     filter_value = 'new_sort'
     sort_value = None
     assert (
@@ -50,7 +50,7 @@ def test__url_params_filter_and_sort_is_none():
     )
 
 
-def test__url_params_filter():
+def url_params_filter__test():
     filter_value = 'new_filter'
     sort_value = 'new_sort'
     assert (

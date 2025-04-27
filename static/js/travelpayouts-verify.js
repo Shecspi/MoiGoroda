d2233f305430d5bb8cd759b89096e67b38969198
Shecspi <shecspi@yandex.ru>
2025-04-27 23:39:48 +0300

    Добавлена рекламная ссылка в сайдбар

---
+++ static/js/travelpayouts-verify.js
@@ -0,0 +1,6 @@
+(function () {
+    var script = document.createElement("script");
+    script.async = 1;
+    script.src = 'https://mn-tz.com/NDExMDA0.js?t=411004';
+    document.head.appendChild(script);
+})();

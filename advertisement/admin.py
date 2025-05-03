88c251fbc0831a2bacb4e873c62aad5c3dc4f2cf
Shecspi <shecspi@yandex.ru>
2025-05-03 14:57:52 +0300

    Добавлены рекламные блоки

--- advertisement/admin.py
+++ advertisement/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from advertisement.models import LinkAdvertisement
+from advertisement.models import LinkAdvertisement, AdvertisementException
 
 
 @admin.register(LinkAdvertisement)
 class AdvertisementAdmin(admin.ModelAdmin):
     list_display = ('title', 'url', 'legal_marking', 'icon_class', 'color')
     search_fields = ('title',)
+
+
+@admin.register(AdvertisementException)
+class AdvertisementExceptionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'deadline')
+    search_fields = ('user',)

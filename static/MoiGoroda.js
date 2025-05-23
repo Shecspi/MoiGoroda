83ca2075277eb2c00012b6de4323f3b0604d121c
Shecspi <shecspi@yandex.ru>
2025-05-23 11:37:51 +0300

    Удален MoiGoroda.js

--- static/MoiGoroda.js
+++
@@ -1,6 +0,0 @@
-$('#sidebar-button-open').on('click', function() {
-    $('.sidebar').addClass('active');
-})
-$('#sidebar-button-close').on('click', function() {
-    $('.sidebar').removeClass('active');
-})

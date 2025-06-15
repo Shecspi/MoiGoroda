4630a67f5ca2bd72c7b991b955092137aa54afb7
Shecspi <shecspi@yandex.ru>
2025-06-15 20:30:06 +0300

    Исправлена ошибка, из-за которой не работала кнопка "Подписаться/Отписаться"

--- frontend/js/entries/subscribe_api.js
+++ frontend/js/entries/subscribe_api.js
@@ -1,16 +1,4 @@
-function getCookie(c_name) {
-    if (document.cookie.length > 0) {
-        c_start = document.cookie.indexOf(c_name + "=");
-        if (c_start != -1) {
-            c_start = c_start + c_name.length + 1;
-            c_end = document.cookie.indexOf(";", c_start);
-            if (c_end == -1) c_end = document.cookie.length;
-            return unescape(document.cookie.substring(c_start, c_end));
-        }
-    }
-    return "";
-}
-
+import {getCookie} from "../components/get_cookie";
 
 async function send_to_server() {
     const subscribe_button = document.getElementById('subscribe_button')
@@ -73,3 +61,6 @@ async function send_to_server() {
 
     return false;
 }
+
+document.getElementById('unsubscribe_button').addEventListener('click', () => send_to_server());
+document.getElementById('subscribe_button').addEventListener('click', () => send_to_server());

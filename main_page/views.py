59cf50b1dc599b2129c52a636446524c5b20326d
Shecspi <shecspi@yandex.ru>
2025-09-05 10:48:09 +0300

    Убран редирект с главной страницы для неавторизованных пользователей

--- main_page/views.py
+++ main_page/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 
 
 def index(request):
     if request.user.is_authenticated:
         return redirect('city-all-list')
     else:
-        return redirect('region-all-list')
+        return render(
+            request,
+            'index.html',
+            context={
+                'page_title': 'Сервис учёта посещённых городов «Мои города»',
+                'page_description': "«Мои города» — сервис учёта посещённых городов: отмечайте города и страны, смотрите их на карте, открывайте новые направления и следите за поездками друзей",
+            },
+        )

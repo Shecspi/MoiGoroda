26c450f24c8633d525b8ff896b497b969a13426a
Shecspi <shecspi@yandex.ru>
2025-09-27 10:19:45 +0300

    Добавлено поле поиска в тулбар списка городов

---
+++ frontend/js/entries/city_search.js
@@ -0,0 +1,61 @@
+import autoComplete from "@tarekraafat/autocomplete.js";
+import { pluralize } from "../components/search_services.js";
+
+window.onload = async () => {
+    const inputEl = document.querySelector("#city-search");
+    const config = {
+        selector: () => inputEl,
+        placeHolder: "Начните вводить название города...",
+        debounce: 300,
+        data: {
+            src: async () => {
+                const query = inputEl.value;
+
+                if (!query) return [];
+
+                let url = `/api/city/search?query=${encodeURIComponent(query)}`;
+
+                const response = await fetch(url);
+                const data = await response.json();
+                return data.map(item => ({ value: item.title, id: item.id }));
+            },
+            keys: ["value"]
+        },
+        resultsList: {
+            render: true,
+            element: (list, data) => {
+                const info = document.createElement("p");
+                info.classList.add("dropdown-item", "my-2", "px-2", "small", "text-secondary");
+                if (data.results.length) {
+                    const word = pluralize(data.results.length, "совпадение", "совпадения", "совпадений");
+                    info.innerHTML = `Найдено <strong>${data.results.length}</strong> ${word} для <strong>"${data.query}"</strong>`;
+                } else {
+                    info.innerHTML = `Найдено <strong>${data.matches.length}</strong> совпадений для <strong>"${data.query}"</strong>`;
+                }
+                list.prepend(info);
+            },
+            maxResults: undefined,
+            noResults: true,
+            destination: () => inputEl,
+            position: "afterend",
+        },
+        resultItem: {
+            highlight: true,
+            element: (item, data) => {
+                item.style = "display: flex; justify-content: space-between;";
+                item.innerHTML = `<span style="text-overflow: ellipsis; white-space: nowrap; overflow: hidden;">${data.match}</span>`;
+            },
+        },
+        events: {
+            input: {
+                selection: event => {
+                    const selection = event.detail.selection.value;
+                    inputEl.value = selection.value;
+                    window.location.href = `/city/${selection.id}`;
+                }
+            }
+        }
+    }
+
+    new autoComplete(config);
+}

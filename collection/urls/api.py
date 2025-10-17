8b0be3117b84988323dee59d3e7d96ab6aa55b3a
Shecspi <shecspi@yandex.ru>
2025-10-17 11:03:41 +0300

    Улучшено форматирование URL для favorite API

--- collection/urls/api.py
+++ collection/urls/api.py
@@ -12,5 +12,9 @@ from collection.api import collection_search, favorite_collection_toggle
 
 urlpatterns = [
     path('search', collection_search, name='collection_search'),
-    path('favorite/<int:collection_id>', favorite_collection_toggle, name='favorite_collection_toggle'),
+    path(
+        'favorite/<int:collection_id>',
+        favorite_collection_toggle,
+        name='favorite_collection_toggle',
+    ),
 ]

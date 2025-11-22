6913b2330121bdedd075ef13be2e1ec04dcb9276
Shecspi <shecspi@yandex.ru>
2025-11-22 16:34:48 +0300

    Рефакторинг карты коллекции: использование общих компонентов
    - Карта коллекции теперь использует create_map() из components/map.js
    - Убрана дублирующая функция addControlsToMap()
    - Все контролы (zoom, fullscreen, screenshot, attribution) теперь добавляются автоматически через create_map()
    - Упрощён код и улучшена читаемость
    - Добавлена обработка случая с пустым списком городов
    - Обеспечено единообразие с картой посещённых городов

--- frontend/js/entries/map_collection.js
+++ frontend/js/entries/map_collection.js
@@ -1,81 +1,35 @@
-import * as L from 'leaflet';
-import 'leaflet-fullscreen';
-import {SimpleMapScreenshoter} from 'leaflet-simple-map-screenshoter';
+/**
+ * Реализует отображение карты коллекции с метками городов.
+ *
+ * ----------------------------------------------
+ *
+ * Copyright © Egor Vavilov (Shecspi)
+ * Licensed under the Apache License, Version 2.0
+ *
+ * ----------------------------------------------
+ */
+
+import L from 'leaflet';
+import {create_map} from '../components/map.js';
+import {icon_visited_pin, icon_not_visited_pin} from '../components/icons.js';
 
 // Массив, хранящий в себе все созданные маркеры.
 // Нужен для того, чтобы отцентрировать и отмасштабировать карту.
 const allMarkers = [];
 
-// Иконка для посещённого пользователем города
-const icon_visited_pin = L.divIcon({
-    className: 'custom-icon-visited-pin',
-    html: '<i class="fa-solid fa-location-dot fs-3 icon-visited-pin" style="color: rgb(90, 170, 90) !important; text-shadow: 0 0 2px #333333;"></i>',
-    iconSize: [21, 28],
-    anchor: [10.5, 28],
-    iconAnchor: [10.5, 28],
-    popupAnchor: [0, -28],
-    tooltipAnchor: [0, -28]
-});
-
-// Иконка для города, который не посетил ни пользователь, ни те, на кого он подписан
-const icon_not_visited_pin = L.divIcon({
-    className: 'custom-icon-not_visited-pin',
-    html: '<i class="fa-solid fa-location-dot fs-3 icon-not-visited-pin" style="color: rgb(210, 90, 90) !important; text-shadow: 0 0 2px #333333;"></i>',
-    iconSize: [21, 28],
-    anchor: [10.5, 28],
-    iconAnchor: [10.5, 28],
-    popupAnchor: [0, -28],
-    tooltipAnchor: [0, -28]
-});
-
-/**
- * Добавляет кнопки приближения и отдаления карты, а также полноэкранного режима.
- */
-function addControlsToMap() {
-    const myAttrControl = L.control.attribution().addTo(map);
-    myAttrControl.setPrefix('<a href="https://leafletjs.com/">Leaflet</a>');
-    L.tileLayer(`${window.TILE_LAYER}`, {
-        maxZoom: 19,
-        attribution: 'Используются карты &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> под лицензией <a href="https://opendatacommons.org/licenses/odbl/">ODbL.</a>'
-    }).addTo(map);
-
-    // Добавляем кнопки приближения и отдаления карты.
-    // Их пришлось удалить и вручную добавить, чтобы перевести текст подсказки на русский.
-    const zoomControl = L.control.zoom({
-      zoomInTitle: 'Нажмите, чтобы приблизить карту',
-      zoomOutTitle: 'Нажмите, чтобы отдалить карту'
-    });
-    zoomControl.addTo(map);
-
-    // Добавляем кнопку полноэкранного режима
-    map.addControl(new L.Control.Fullscreen({
-        title: {
-            'false': 'Полноэкранный режим',
-            'true': 'Выйти из полноэкранного режима'
-        }
-    }));
-
-    // Добавляем кнопку создания скриншота
-    new SimpleMapScreenshoter().addTo(map);
-}
-
-// Массив с городами региона
+// Массив с городами коллекции
 // ['lat', 'lon', 'name', 'is_visited']
 const cities = window.CITIES_DATA || [];
 
-// Отображаем карту на странице
-const map = L.map('map', {
-    attributionControl: false,
-    zoomControl: false
-}).setView([60, 50], 4);
-addControlsToMap();
+// Создаём карту используя общий компонент
+const map = create_map();
 
-// Отображаем на карте все города меняя тип иконки в зависимости от того, посещён город или нет
-for (let i = 0; i < (cities.length); i++) {
-    let lat = cities[i].lat;
-    let lon = cities[i].lon;
-    let name = cities[i].name;
-    let is_visited = cities[i].is_visited;
+// Отображаем на карте все города, меняя тип иконки в зависимости от того, посещён город или нет
+for (let i = 0; i < cities.length; i++) {
+    const lat = cities[i].lat;
+    const lon = cities[i].lon;
+    const name = cities[i].name;
+    const is_visited = cities[i].is_visited;
 
     const icon = (is_visited === true) ? icon_visited_pin : icon_not_visited_pin;
     const marker = L.marker([lat, lon], {icon: icon}).addTo(map);
@@ -85,5 +39,10 @@ for (let i = 0; i < (cities.length); i++) {
 }
 
 // Центрируем и масштабируем карту
-const group = new L.featureGroup([...allMarkers]);
-map.fitBounds(group.getBounds());
+if (allMarkers.length > 0) {
+    const group = new L.featureGroup([...allMarkers]);
+    map.fitBounds(group.getBounds());
+} else {
+    // Если городов нет, устанавливаем вид по умолчанию
+    map.setView([60, 50], 4);
+}

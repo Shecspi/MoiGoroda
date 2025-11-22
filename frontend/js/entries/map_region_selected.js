445beb6f84601f7253a9717ef4c82299d60775f8
Shecspi <shecspi@yandex.ru>
2025-11-22 16:49:23 +0300

    Рефакторинг карты региона и перевод на Preline UI
    - Карта региона теперь использует create_map() из components/map.js
    - Убрана дублирующая функция addControlsToMap()
    - Используются общие иконки из components/icons.js
    - Обновлён дизайн страницы карты региона на Preline UI стиль
    - Обновлён тулбар карты региона на Preline UI с tooltips
    - Бейджик статистики использует зелёный цвет для авторизованных пользователей
    - Обновлено модальное окно помощи на Preline UI структуру
    - Улучшен текст и структура модального окна помощи для лучшей читаемости
    - Заменены иконки Font Awesome на SVG версии (вопрос, список)
    - Кнопка "Закрыть" в модальном окне стилизована серым цветом
    - Обеспечено единообразие с картами коллекции и посещённых городов

--- frontend/js/entries/map_region_selected.js
+++ frontend/js/entries/map_region_selected.js
@@ -1,88 +1,49 @@
-import * as L from 'leaflet';
-import 'leaflet-fullscreen';
-import {SimpleMapScreenshoter} from 'leaflet-simple-map-screenshoter';
+/**
+ * Реализует отображение карты региона с метками городов и границами региона.
+ *
+ * ----------------------------------------------
+ *
+ * Copyright © Egor Vavilov (Shecspi)
+ * Licensed under the Apache License, Version 2.0
+ *
+ * ----------------------------------------------
+ */
 
+import L from 'leaflet';
+import {create_map} from '../components/map.js';
+import {icon_visited_pin, icon_not_visited_pin} from '../components/icons.js';
+
+// Стили для полигона региона
 const fillOpacity = 0.1;
 const fillColor = '#6382ff';
 const strokeColor = '#0033ff';
 const strokeOpacity = 0.3;
 const strokeWidth = 2;
-const iso3166_code = document.getElementById('iso3166_code').dataset.iso3166_code
+
+// Получаем код региона из DOM
+const iso3166_code = document.getElementById('iso3166_code').dataset.iso3166_code;
 const region_code = iso3166_code.split('-')[1];
 const country_code = iso3166_code.split('-')[0];
 
-let map;
+// Массив с городами региона
+// ['latitude', 'longitude', 'city name', 'is_visited']
+const all_cities = window.ALL_CITIES || [];
 
-/**
- * Добавляет кнопки приближения и отдаления карты, а также полноэкранного режима.
- */
-function addControlsToMap() {
-    const myAttrControl = L.control.attribution().addTo(map);
-    myAttrControl.setPrefix('<a href="https://leafletjs.com/">Leaflet</a>');
-    L.tileLayer(`${window.TILE_LAYER}`, {
-        maxZoom: 19,
-        attribution: 'Используются карты &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> под лицензией <a href="https://opendatacommons.org/licenses/odbl/">ODbL.</a>'
-    }).addTo(map);
-
-    // Добавляем кнопки приближения и отдаления карты.
-    // Их пришлось удалить и вручную добавить, чтобы перевести текст подсказки на русский.
-    const zoomControl = L.control.zoom({
-      zoomInTitle: 'Нажмите, чтобы приблизить карту',
-      zoomOutTitle: 'Нажмите, чтобы отдалить карту'
-    });
-    zoomControl.addTo(map);
-
-    // Добавляем кнопку полноэкранного режима
-    map.addControl(new L.Control.Fullscreen({
-        title: {
-            'false': 'Полноэкранный режим',
-            'true': 'Выйти из полноэкранного режима'
-        }
-    }));
-}
-
-let all_cities = window.ALL_CITIES;
-
-map = L.map('map', {
-    attributionControl: false,
-    zoomControl: false,
-    center: [55.751244, 37.618423],
-    zoom: 3
-});
-addControlsToMap();
-new SimpleMapScreenshoter().addTo(map);
-
+// Массив, хранящий в себе все созданные маркеры.
+// Нужен для того, чтобы отцентрировать и отмасштабировать карту.
 const allMarkers = [];
 
-// Отображаем на карте все города,
-// меняя цвет иконки в зависимости от того, посещён город или нет
-for (let i = 0; i < (all_cities.length); i++) {
-    let coordinateWidth = all_cities[i][0];
-    let coordinateLongitude = all_cities[i][1];
-    let city = all_cities[i][2];
+// Создаём карту используя общий компонент
+const map = create_map();
 
-    // TODO: Использовать глобальные иконки без дублирования кода
-    // Иконка для посещённого пользователем города
-    const icon_visited_pin = L.divIcon({
-        className: 'custom-icon-visited-pin',
-        html: '<i class="fa-solid fa-location-dot fs-3 icon-visited-pin" style="color: rgb(90, 170, 90) !important; text-shadow: 0 0 2px #333333;"></i>',
-        iconSize: [21, 28],
-        anchor: [10.5, 28],
-        iconAnchor: [10.5, 28],
-        popupAnchor: [0, -28],
-        tooltipAnchor: [0, -28]
-    });
-    // Иконка для города, который не посетил ни пользователь, ни те, на кого он подписан
-    const icon_not_visited_pin = L.divIcon({
-        className: 'custom-icon-not_visited-pin',
-        html: '<i class="fa-solid fa-location-dot fs-3 icon-not-visited-pin" style="color: rgb(210, 90, 90) !important; text-shadow: 0 0 2px #333333;"></i>',
-        iconSize: [21, 28],
-        anchor: [10.5, 28],
-        iconAnchor: [10.5, 28],
-        popupAnchor: [0, -28],
-        tooltipAnchor: [0, -28]
-    });
-    const icon = (all_cities[i][3] === true) ? icon_visited_pin : icon_not_visited_pin;
+// Отображаем на карте все города, меняя тип иконки в зависимости от того, посещён город или нет
+for (let i = 0; i < all_cities.length; i++) {
+    const coordinateWidth = all_cities[i][0];
+    const coordinateLongitude = all_cities[i][1];
+    const city = all_cities[i][2];
+    const is_visited = all_cities[i][3] === true;
+
+    const icon = is_visited ? icon_visited_pin : icon_not_visited_pin;
     const marker = L.marker([coordinateWidth, coordinateLongitude], {icon: icon}).addTo(map);
     marker.bindTooltip(city, {direction: 'top'});
 
@@ -90,13 +51,13 @@ for (let i = 0; i < (all_cities.length); i++) {
 }
 
 // Загружаем полигон региона
-const url = `${window.URL_GEO_POLYGONS}/region/hq/${country_code}/${region_code}`
+const url = `${window.URL_GEO_POLYGONS}/region/hq/${country_code}/${region_code}`;
 fetch(url)
     .then(response => {
         if (!response.ok) {
-            throw new Error(response.statusText)
+            throw new Error(response.statusText);
         }
-        return response.json()
+        return response.json();
     })
     .then(geoJson => {
         const myStyle = {
@@ -109,6 +70,8 @@ fetch(url)
         const geojson = L.geoJSON(geoJson, {
             style: myStyle,
         }).addTo(map);
+        
+        // Если нет маркеров, центрируем карту по границам региона
         if (allMarkers.length === 0) {
             map.fitBounds(geojson.getBounds());
         }
@@ -117,7 +80,7 @@ fetch(url)
         console.log('Произошла ошибка при загрузке границ региона:\n' + error);
     });
 
-// Центрируем и масштабируем карту
+// Центрируем и масштабируем карту по маркерам городов
 if (allMarkers.length > 0) {
     const group = new L.featureGroup([...allMarkers]);
     map.fitBounds(group.getBounds());

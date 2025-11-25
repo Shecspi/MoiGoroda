2dcf31f4041b6e4919ad28d7cea74a499d2ce8c8
Shecspi <shecspi@yandex.ru>
2025-11-25 16:31:49 +0300

    CI: collectstatic теперь bash-скрипт

--- scripts/collectstatic.sh
+++ scripts/collectstatic.sh
@@ -1,7 +1,6 @@
-#!/usr/bin/expect
-# Скрипт собирает статику для Nginx
-set timeout -1
-spawn poetry run python3 manage.py collectstatic
-expect "Are you sure you want to do this?"
-send "yes\r"
-expect eof
+#!/usr/bin/env bash
+set -euo pipefail
+
+echo "Сборка статики Django..."
+poetry run python3 manage.py collectstatic --noinput
+echo "Готово."

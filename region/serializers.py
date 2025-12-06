d5aac0388e131623443e06912a8e86687bd55403
Shecspi <shecspi@yandex.ru>
2025-12-06 15:23:08 +0300

    Добавлен выбор регионов с отображением названия страны
    - Обновлен API region_list_by_country для поддержки множественного выбора стран (параметр country_ids)
    - Добавлено поле country_name в RegionSerializer для отображения названия страны
    - Реализован Preline Select для выбора регионов с множественным выбором
    - Регионы отображаются в формате 'Название региона, Название страны'
    - Регионы сортируются по full_name
    - Регионы загружаются при выборе одной или нескольких стран

--- region/serializers.py
+++ region/serializers.py
@@ -4,13 +4,29 @@ from .models import Region
 
 class RegionSerializer(serializers.ModelSerializer):  # type: ignore[type-arg]
     title = serializers.SerializerMethodField()
+    country_name = serializers.SerializerMethodField()
 
     class Meta:
         model = Region
-        fields = ['id', 'title']
+        fields = ['id', 'title', 'country_name']
 
     def get_title(self, obj: Region) -> str:
-        return str(obj)
+        """
+        Возвращает полное название региона.
+
+        :param obj: Экземпляр модели Region
+        :return: Полное название региона
+        """
+        return obj.full_name
+
+    def get_country_name(self, obj: Region) -> str:
+        """
+        Возвращает название страны.
+
+        :param obj: Экземпляр модели Region
+        :return: Название страны
+        """
+        return obj.country.name
 
 
 class RegionSearchParamsSerializer(serializers.Serializer):  # type: ignore[type-arg]

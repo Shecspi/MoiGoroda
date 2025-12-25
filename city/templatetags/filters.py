640fbc89924afaf1d8f5beea94c981bf4fd7b35d
Shecspi <shecspi@yandex.ru>
2025-12-25 15:37:31 +0300

    Исправлены индексы slice и добавлен фильтр startswith для проверки типов сортировки
    - Заменены все slice с индексами на фильтр startswith в шаблоне filter_panel.html
    - Добавлен кастомный template filter startswith в city/templatetags/filters.py
    - Исправлена проблема с неправильными индексами для проверки типов сортировки
    - Код стал проще и понятнее, не требует подсчёта символов

--- city/templatetags/filters.py
+++ city/templatetags/filters.py
@@ -1,3 +1,12 @@
+"""
+----------------------------------------------
+
+Copyright © Egor Vavilov (Shecspi)
+Licensed under the Apache License, Version 2.0
+
+----------------------------------------------
+"""
+
 from typing import Any
 
 from django import template
@@ -8,3 +17,17 @@ register = template.Library()
 @register.filter
 def in_list(value: Any, arg: str) -> bool:
     return value in arg.split(',')
+
+
+@register.filter
+def startswith(value: str | None, arg: str) -> bool:
+    """
+    Проверяет, начинается ли строка с указанного префикса.
+
+    :param value: Строка для проверки
+    :param arg: Префикс для проверки
+    :return: True, если строка начинается с префикса, иначе False
+    """
+    if value is None:
+        return False
+    return str(value).startswith(str(arg))

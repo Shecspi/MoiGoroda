4f3258b845c789acbe6118e5af85ec893a97d59b
Shecspi <shecspi@yandex.ru>
2026-02-08 14:07:08 +0300

    Убран depreceted параметр из settings.py

--- MoiGoroda/settings.py
+++ MoiGoroda/settings.py
@@ -386,13 +386,17 @@ CKEDITOR_5_CONFIGS = {
 }
 
 # Django Storage
-
-DEFAULT_FILE_STORAGE = os.getenv('DEFAULT_FILE_STORAGE')
-
+STORAGES = {
+    'default': {
+        'BACKEND': 'storages.backends.s3boto3.S3Boto3Storage',
+    },
+    'staticfiles': {
+        'BACKEND': 'django.contrib.staticfiles.storage.StaticFilesStorage',
+    },
+}
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
-
 AWS_DEFAULT_ACL = None
 AWS_QUERYSTRING_AUTH = False

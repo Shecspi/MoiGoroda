68d214446f93c78cb5bc03597d6fd25a0f62892d
Shecspi <shecspi@yandex.ru>
2026-05-29 16:00:39 +0300

    Добавлено модельное окно с информацией о том, что это за страница и как ей пользоваться

--- geo_polygons/views.py
+++ geo_polygons/views.py
@@ -5,5 +5,14 @@ from django.views.generic import TemplateView
 class GeoPolygonsViewer(TemplateView):
     template_name = 'geo_polygons/page.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = 'Полигоны OpenStreetMap'
+        context['page_description'] = (
+            'Просмотр и скачивание полигонов объектов OpenStreetMap в выбранной точке на карте'
+        )
+        context['active_page'] = 'geo_polygons'
+        return context
+
     def get(self, request: HttpRequest, *args, **kwargs):
         return super().get(request, *args, **kwargs)
